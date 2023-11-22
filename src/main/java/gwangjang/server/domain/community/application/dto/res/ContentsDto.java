dafc63a73d2d12d2c67eadd22524b75cda160fb1
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-11-22 22:28:58 +0900

    fix : ContentsDto 변경

--- src/main/java/gwangjang/server/domain/community/application/dto/res/ContentsDto.java
+++ src/main/java/gwangjang/server/domain/community/application/dto/res/ContentsDto.java
@@ -26,7 +26,7 @@ public class ContentsDto {
     private String issueTitle;
     private String keyword;
     private String pubDate;
-    private String topicTitle;
+    private String topic;
     private String imgUrl;
 
 
