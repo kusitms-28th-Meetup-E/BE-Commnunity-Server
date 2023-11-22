bb21daf5775a4d81250c4a38f2d4ad214b463621
eojin0814 <leeeojin6@gmail.com>
2023-11-22 21:44:43 +0900

    feat: Search API

--- src/main/java/gwangjang/server/domain/community/domain/service/CommunityQueryService.java
+++ src/main/java/gwangjang/server/domain/community/domain/service/CommunityQueryService.java
@@ -2,6 +2,7 @@ package gwangjang.server.domain.community.domain.service;
 
 import gwangjang.server.domain.community.application.dto.res.CommunityRes;
 import gwangjang.server.domain.community.application.dto.res.MemberDto;
+import gwangjang.server.domain.community.application.dto.res.SearchRes;
 import gwangjang.server.domain.community.application.mapper.CommunityMapper;
 import gwangjang.server.domain.community.domain.entity.Community;
 import gwangjang.server.domain.community.domain.entity.constant.CommunityOrderCondition;
@@ -12,7 +13,10 @@ import gwangjang.server.global.feign.client.FindMemberFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 //@DomainService
 @Service
@@ -21,6 +25,8 @@ public class CommunityQueryService {
 
     private final CommunityRepository communityRepository;
     private final FindMemberFeignClient findMemberFeignClient;
+
+    private final CommunityMapper communityMapper = new CommunityMapper();
 
     public Community getCommunityById(Long communityId) {
         return communityRepository.findById(communityId).orElseThrow(NotFoundCommunityException::new
@@ -82,5 +88,17 @@ public class CommunityQueryService {
 
     public List<CommunityRes> getCommunityByMyHearts(String memberId) {
         return communityRepository.findCommunityByMyHearts(memberId).orElseThrow(NotFoundCommunityException::new);
+    }
+    public SearchRes search(String memberId, CommunityOrderCondition communityOrderCondition, String keyword) {
+        List<CommunityRes> searchResults = communityRepository.getSearchCommunity(memberId,communityOrderCondition,keyword).orElseThrow(NotFoundCommunityException::new);;
+
+
+        SearchRes searchRes = new SearchRes();
+        searchRes.setSearchKeyword(keyword);
+        searchRes.setSearchCount(String.valueOf(searchResults.size()));
+        searchRes.setCommunityResList(searchResults);
+
+        return searchRes;
+
     }
 }
